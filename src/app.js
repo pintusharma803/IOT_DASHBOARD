d29dfa33568b605a38b9da64a596984a5d18a4f9
Pintu Kumar Sharma <148368824+pintusharma803@users.noreply.github.com>
2026-06-27 14:02:39 +0530

    IOT Dashboard

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const User = require("../src/models/User");
 const middleware = require("../src/middleware/authMiddleware");
 const cookieParser = require("cookie-parser");
 
